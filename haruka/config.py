368a321b67c737bdc1065d1606393c674678b1e2
midhunkm1294 <46639881+midhunkm1294@users.noreply.github.com>
2020-05-26 10:50:35 +0530

    Update config.py

--- haruka/config.py
+++ haruka/config.py
@@ -12,7 +12,7 @@ class Config(object):
     # REQUIRED
     API_KEY = ""
     OWNER_ID = "1116223246"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "CRAZYHAX"
+    OWNER_USERNAME = "Mariodevs"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = ''  # needed for any database modules
@@ -20,7 +20,7 @@ class Config(object):
     LOAD = []
     NO_LOAD = ['translation', 'sed']
     WEBHOOK = ANYTHING
-    URL = "https://itsflashbot.herokuapp.com"
+    URL = "https://mrsfridayz.herokuapp.com"
 
     # OPTIONAL
     SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
